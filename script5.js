af6b08253f919947b73bef4afb677e957659540f
btharunaisvpcet-source <b.tharunaisvpcet@gmail.com>
2026-07-01 18:22:10 +0530

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,14 @@
+function greet() {
+    document.getElementById("output").textContent =
+    "Hello from Function Declaration!";
+}
+
+function sayGoodbye() {
+    document.getElementById("output").textContent =
+    "Goodbye from Function Expression!";
+}
+
+function showThanks() {
+    document.getElementById("output").textContent =
+    "Thanks from Arrow Function!";
+}
